ac00b0d66115e8b717c2447f5b4292606a5b8479
w3b3ye <w3b3ye@gmail.com>
2023-12-28 10:21:19 +0000

    Update script for "variable".

--- 2_5_variables/script.js
+++ 2_5_variables/script.js
@@ -1,55 +1,63 @@
-// var, let, const
+// Ways to declare a variable
+// `var`, `let`, & `const`
 
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+let firstName = 'John';
+const lastName = 'Doe';
+let age = 30;
 
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
+console.log(age);
 
-// // variable names can have letters, numbers, _, $
-// // Can not start with number
+// Naming Conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
 
-// // Multi word vars
-// var firstName = 'John'; // Camel case
-// var first_name = 'Sara'; // Underscore
-// var FirstName = 'Tom'; // Pascal case
-// var firstname;
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
 
-// LET
-// let name;
-// name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// We can re-assign `let` variables. If you change `age` to use `const`, you will get an error
+age = 31;
+console.log(age);
 
+// With let, we can declare a value without assigning a value
+let score;
+score = 1;
+console.log(score);
 
-// // CONST
-// const name = 'John';
-// console.log(name);
-// // Can not reassign
-// name = 'Sara';
-// // Have to assign a value
-// // const greeting;
+if (true) {
+  score = score + 1;
+}
 
-// const person = {
-//   name: 'John',
-//   age: 30
-// }
+console.log(score);
 
-// person.name = 'Sara';
-// person.age = 32;
+const x = 100;
 
-// console.log(person);
+// We can not re-assign a const variable
+// x = 200 // Will result in an error
 
-// const numbers = [1, 2, 3, 4, 5];
-// numbers.push(6);
+// We can still manipulate arrays and objects using const
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
 
-// console.log(numbers);
+const person = {
+  name: 'Brad',
+};
+person.name = 'John';
+person.email = 'brad@gmail.com';
+console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);
 
 const person = {
   name: 'John',
