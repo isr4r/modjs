bfe7094a2cbc6d93315a547ce262be2f7ef1ed95
w3b3ye <w3b3ye@gmail.com>
2024-01-06 07:06:04 +0000

    Chapter 2 - Completed

---
+++ 2_15_number_challenge/script.js
@@ -0,0 +1,19 @@
+
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+//let sumOutput, diffOutput, prodOutput, quotOutput, rmOutput;
+//sumOutput = x + y;
+console.log(x);
+console.log(y);
+
+
+console.log(`Sum: ${x} + ${y} = ${x + y}`);
+console.log(`Difference: ${x} - ${y} = ${x - y}`);
+console.log(`Product: ${x} * ${y} = ${x * y}`);
+console.log(`Quotient: ${x} / ${y} = ${x / y}`);
+console.log(`Remainder: ${x} % ${y} = ${x % y}`);
+
+
+
+
